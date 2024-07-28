08bf52497c78ff136c4e1329a0b3cdbcbc60007e
rahulgzb <rahulmehta.gzb@email.com>
2024-07-28 14:41:08 +0530

    addec

--- genarate_plots.py
+++ genarate_plots.py
@@ -18,7 +18,8 @@ def aggregate_metrics(metrics_list):
         "rouge2": [],
         "rougeL": [],
         "bleu": [],
-        "avg_val_loss": []
+        "avg_val_loss": [],
+        "avg_train_loss": []
     }
     
     for metrics in metrics_list:
@@ -27,7 +28,8 @@ def aggregate_metrics(metrics_list):
         aggregated_metrics["rougeL"].append(metrics["rougeL"])
         aggregated_metrics["bleu"].append(metrics["bleu"])
         aggregated_metrics["avg_val_loss"].append(metrics["avg_val_loss"])
-    
+        aggregated_metrics["avg_train_loss"].append(metrics["avg_train_loss"])
+        
     return aggregated_metrics
 
 def save_plot(args):
@@ -43,22 +45,36 @@ def save_plot(args):
     ax1.set_xlabel('Epoch')
     ax1.set_ylabel('Loss')
     ax1.plot(aggregated_metrics["avg_val_loss"], label='Validation Loss', color='tab:blue')
+    ax1.plot(aggregated_metrics["avg_train_loss"], label='train Loss', color='tab:green')
     ax1.tick_params(axis='y', labelcolor='tab:blue')
+    # Adding legends
+    fig.legend(loc='upper left', bbox_to_anchor=(0.1,0.9))
+
+    # Saving the plot
+    plt.title('loss Over Epochs')
+    plt.grid(True)
+    plt.savefig('loss_plot.jpg', format='jpg')
+    plt.show()
+    plt.close()
+
+
+
     # Create a second y-axis to plot ROUGE and BLEU scores
-    ax2 = ax1.twinx()
+    ax2 = ax1
     ax2.set_ylabel('Scores')
     ax2.plot(aggregated_metrics["rouge1/f1 score"], label='ROUGE-1/F1 Score', color='tab:orange')
     ax2.plot(aggregated_metrics["rouge2"], label='ROUGE-2', color='tab:green')
     ax2.plot(aggregated_metrics["rougeL"], label='ROUGE-L', color='tab:red')
     ax2.plot(aggregated_metrics["bleu"], label='BLEU', color='tab:purple')
     ax2.tick_params(axis='y', labelcolor='black')
-
     # Adding legends
     fig.legend(loc='upper left', bbox_to_anchor=(0.1,0.9))
 
     # Saving the plot
-    plt.title('Metrics Over Epochs')
+    plt.title('matrix Over Epochs')
     plt.grid(True)
-    plt.savefig('metrics_plot.jpg', format='jpg')
+    plt.savefig('matrix_plot.jpg', format='jpg')
     plt.show()
     plt.close()
+
+    
